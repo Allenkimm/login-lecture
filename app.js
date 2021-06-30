45693b7f98d7eaa0238f766c44624785c1ef50ea
Ubuntu <khkim@backend-test.k3g0lgiethtujhzrfkdavocd0a.syx.internal.cloudapp.net>
2021-06-30 05:40:49 +0000

    프런트 단 javascript 만들기

---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+
+
+// 라우팅
+const home = require("./src/routes/home");
+
+
+// 앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+//js 폴더에 접근하기위해 등록하는 미들웨어
+app.use(express.static(`${__dirname}/src/public`));
+app.use("/", home); // use -> 미들 웨어를 등록해주는 메소드.
+
+module.exports = app;
