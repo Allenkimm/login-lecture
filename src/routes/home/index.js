a089e8d08c838b2b76bfcf01c66d75ca3503edf4
Ubuntu <khkim@backend-test.k3g0lgiethtujhzrfkdavocd0a.syx.internal.cloudapp.net>
2021-06-30 02:23:51 +0000

    깃 저장소 초기화

---
+++ src/routes/home/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+
+// 컨트롤러 지정
+const ctrl = require("./home.ctrl");
+
+
+router.get("/", ctrl.main );
+
+router.get("/login", ctrl.login );
+
+module.exports = router;
