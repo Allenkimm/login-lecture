c9e7728ec841bc0d46028cf7e1ed72e936adf452
Kwanghyun <khkim@kismi.kr>
2021-07-02 04:53:50 +0000

    user model까지 완료

--- src/routes/home/home.ctrl.js
+++ src/routes/home/home.ctrl.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
     home: (req, res)=> {
@@ -15,26 +16,30 @@ const output = {
 
 const process = {
     login: (req, res) => {
-       const id = req.body.id,
-        psword = req.body.psword;
+       const user = new User(req.body);
+        const response = user.login();
+        console.log(response);
+    // const users = UserStorage.getUsers("id","psword", "name");
+    
+    // // new 라는 
+    // //const users = new UserStorage();
+    // // users.getUsers()
 
-    const users = UserStorage.getUsers("id","psword", "name");
-
-        const response = {};
-        if(users.id.includes(id)){
-            const idx = users.id.indexOf(id);
-            if (users.psword[idx] === psword) {
-                response.success  = true;
-                return res.json(response);
+    //     const response = {};
+    //     if(users.id.includes(id)){
+    //         const idx = users.id.indexOf(id);
+    //         if (users.psword[idx] === psword) {
+    //             response.success  = true;
+    //             return res.json(response);
                     
-            }
+    //         }
             
-        }
+    //     }
         
         
-        response.success = false;
-        response.msg = "로그인에 실패"
-        return res.json(response);
+        // response.success = false;
+        // response.msg = "로그인에 실패"
+        // return res.json(response);
 
         
     },
@@ -44,5 +49,7 @@ module.exports = {
    output,
    process,
 }
+
+
 
 
